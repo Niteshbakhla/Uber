891b28e845d7128361856351557c528651479f61
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-11-29 21:12:12 +0530

    basic setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,14 @@
-const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config();
 
-const app = express();
 
+const http = require("http")
+const app = require("./server")
+const PORT = process.env.PORT || 5000
+const cors = require("cors")
 
-app.get("/", () => { })
+app.use(cors());
+
+app.listen(PORT, () => {
+            console.log(`Server is running at ${PORT}`)
+})
