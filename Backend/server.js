3b5c907dd15f882e97ee9526460bbd6a11668aa5
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-11-30 20:20:58 +0530

    added a middleware in express so that I express understand the json format document.

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,6 @@
 const express = require("express")
 const app = express();
 
-app.get("/", (req, res) => {
-            res.send("Hey this is working");
-});
 
-
+app.use(express.json())
 module.exports = app
