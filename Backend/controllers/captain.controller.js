d7d89e3d455f6cde31c5883c34bbc8081e0abc7d
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-12-05 19:26:22 +0530

    created a loginCaptain funtion

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -38,3 +38,56 @@ exports.registerCaptain = async (req, res) => {
                         return res.status(500).json({ success: false, message: error.message })
             }
 }
+
+
+exports.loginCaptain = async (req, res, next) => {
+            try {
+                        const errors = validationResult(req);
+                        if (!errors.isEmpty()) {
+                                    return res.status(400).json({ errors: errors.array() });
+                        }
+
+                        const { email, password } = req.body;
+
+                        const captain = await captainModel.findOne({ email }).select('+password');
+
+                        if (!captain) {
+                                    return res.status(401).json({ message: 'Invalid email or password' });
+                        }
+
+                        const isMatch = await captain.comparePassword(password);
+
+                        if (!isMatch) {
+                                    return res.status(401).json({ message: 'Invalid email or password' });
+                        }
+
+                        const token = captain.generateAuthToken();
+
+                        res.cookie('token', token);
+
+                        return res.status(200).json({ token, captain, message: "Login successfully" });
+            } catch (error) {
+                        return res.status(500).json({ message: error.message });
+            }
+}
+
+
+exports.getCaptainProfile = async (req, res, next) => {
+            return res.status(200).json({ captain: req.captain });
+}
+
+
+
+exports.logoutCaptain = async (req, res, next) => {
+            try {
+                        const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+                        await blackListTokenModel.create({ token });
+
+                        res.clearCookie('token');
+
+                        return res.status(200).json({ message: 'Logout successfully' });
+            } catch (error) {
+                        return res.status(500).json({ success: false, message: error.message })
+            }
+}
