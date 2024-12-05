a45bad807a5d62fdb4da9b9a26e95e197b6f6290
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-12-05 18:49:08 +0530

    just commented it the validation wheather is blacklist or not

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -3,27 +3,29 @@ const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken");
 const blacklistModel = require("../Models/blacklist.model");
 
-
-
 exports.authUser = async (req, res, next) => {
+
             const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+         
             if (!token) {
                         return res.status(401).json({ message: "Unauthorized" });
             }
 
-            const isBlacklist = await userModel.findOne({ token: token });
-            if (!isBlacklist) {
-                        return res.status(401).json({ message: "Unauthorized" });
-            }
+            // const isBlacklist = await userModel.findOne({ token: token });
+            // if (!isBlacklist) {
+            //             return res.status(401).json({ message: "Unauthorized" });
+            // }
             try {
 
                         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
                         const user = await userModel.findById(decoded._id);
                         req.user = user;
+                 
                         return next();
             } catch (error) {
 
                         return res.status(401).json({ message: error.message });
             }
 }
+
