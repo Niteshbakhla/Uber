4b5e947c31aad478586882012b6938bebc2faa96
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-12-09 22:40:11 +0530

    here is getting the captain profile

--- frontend/src/protectedroues/CaptainProtected.jsx
+++ frontend/src/protectedroues/CaptainProtected.jsx
@@ -1,9 +1,11 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { CaptainDataContext } from '../context/CaptainContext';
+import axios from 'axios';
 
 const CaptainProtected = ({ children }) => {
 
+            const [isLoading, setIsLoading] = useState(true)
 
             const navigate = useNavigate();
 
@@ -11,11 +13,32 @@ const CaptainProtected = ({ children }) => {
 
             const { captain, setCaptain } = useContext(CaptainDataContext);
 
+
             useEffect(() => {
                         if (!token) {
                                     return navigate("/captain-login");
                         }
-            }, [token, navigate]);
+            }, [token]);
+
+            axios.get(`${import.meta.env.VITE_BASE_URL}/captain/profile`, {
+                        headers: {
+                                    Authorization: `Bearer ${token}`,
+                        },
+
+            }).then(res => {
+                        if (res.status === 200) {
+                                    setCaptain(res.data.captain)
+                                    setIsLoading(false)
+                        }
+            }).catch(err => {
+                        console.log(err)
+                        localStorage.removeItem("token")
+                        navigate("/captain-login")
+            })
+
+            if (isLoading) {
+                        return <div>Loading.....</div>
+            }
             return (
                         <>
                                     {children}
