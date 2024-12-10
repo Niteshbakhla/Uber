5f2d3586932bd211319bc4a5e9d4948a3c35581f
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-12-10 21:30:59 +0530

    created a location search

---
+++ frontend/src/pages/LocationSearch.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const LocationSearch = () => {
+            return (
+                        <div>
+
+                                    <div className=" p-4 flex items-center my-2 justify-center gap-2 pt-10">
+                                                <h2 className=' text-2xl w-10 h-10  bg-[#eee] rounded-full
+                                                 p-4 flex justify-center items-center'><i class="ri-map-pin-fill"></i></h2>
+                                                <h4 className='text-4xl  font-semibold'>24B, Near Kapoor's cafe, Sheriyans Coding School, Bhopal</h4>
+                                    </div>
+
+                                    <div className=" p-4 flex items-center my-2 justify-center gap-2 pt-10">
+                                                <h2 className=' text-2xl w-10 h-10  bg-[#eee] rounded-full
+                                                 p-4 flex justify-center items-center'><i class="ri-map-pin-fill"></i></h2>
+                                                <h4 className='text-4xl  font-semibold'>24B, Near Kapoor's cafe, Sheriyans Coding School, Bhopal</h4>
+                                    </div>
+
+                                    <div className=" p-4 flex items-center my-2 justify-center gap-2 pt-10">
+                                                <h2 className=' text-2xl w-10 h-10  bg-[#eee] rounded-full
+                                                 p-4 flex justify-center items-center'><i class="ri-map-pin-fill"></i></h2>
+                                                <h4 className='text-4xl  font-semibold'>24B, Near Kapoor's cafe, Sheriyans Coding School, Bhopal</h4>
+                                    </div>
+
+                                    <div className=" p-4 flex items-center my-2 justify-center gap-2 pt-10">
+                                                <h2 className=' text-2xl w-10 h-10  bg-[#eee] rounded-full
+                                                 p-4 flex justify-center items-center'><i class="ri-map-pin-fill"></i></h2>
+                                                <h4 className='text-4xl  font-semibold'>24B, Near Kapoor's cafe, Sheriyans Coding School, Bhopal</h4>
+                                    </div>
+
+                        </div>
+            )
+}
+
+export default LocationSearch
