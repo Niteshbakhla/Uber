1d49dd0f0fbeb89430b76f9dec343239d347c9cd
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-12-13 15:32:30 +0530

    created a array and rendered dynamically

--- frontend/src/pages/LocationSearch.jsx
+++ frontend/src/pages/LocationSearch.jsx
@@ -1,35 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-const LocationSearch = () => {
+const LocationSearch = ({ setPanel }) => {
+            // Array of locations
+            const locations = [
+                        '24B, Near Kapoor\'s cafe, Sheriyans Coding School, Bhopal',
+                        '12A, Near City Mall, XYZ Software Solutions, Bhopal',
+                        '56B, Opposite Central Park, ABC Technologies, Bhopal',
+                        '78C, Near Rajmahal Theatre, MNO Corporation, Bhopal'
+            ];
+
             return (
                         <div>
-
-                                    <div className=" p-4 flex items-center my-2 justify-center gap-2 pt-10">
-                                                <h2 className=' text-2xl w-10 h-10  bg-[#eee] rounded-full
-                                                 p-4 flex justify-center items-center'><i class="ri-map-pin-fill"></i></h2>
-                                                <h4 className='text-4xl  font-semibold'>24B, Near Kapoor's cafe, Sheriyans Coding School, Bhopal</h4>
-                                    </div>
-
-                                    <div className=" p-4 flex items-center my-2 justify-center gap-2 pt-10">
-                                                <h2 className=' text-2xl w-10 h-10  bg-[#eee] rounded-full
-                                                 p-4 flex justify-center items-center'><i class="ri-map-pin-fill"></i></h2>
-                                                <h4 className='text-4xl  font-semibold'>24B, Near Kapoor's cafe, Sheriyans Coding School, Bhopal</h4>
-                                    </div>
-
-                                    <div className=" p-4 flex items-center my-2 justify-center gap-2 pt-10">
-                                                <h2 className=' text-2xl w-10 h-10  bg-[#eee] rounded-full
-                                                 p-4 flex justify-center items-center'><i class="ri-map-pin-fill"></i></h2>
-                                                <h4 className='text-4xl  font-semibold'>24B, Near Kapoor's cafe, Sheriyans Coding School, Bhopal</h4>
-                                    </div>
-
-                                    <div className=" p-4 flex items-center my-2 justify-center gap-2 pt-10">
-                                                <h2 className=' text-2xl w-10 h-10  bg-[#eee] rounded-full
-                                                 p-4 flex justify-center items-center'><i class="ri-map-pin-fill"></i></h2>
-                                                <h4 className='text-4xl  font-semibold'>24B, Near Kapoor's cafe, Sheriyans Coding School, Bhopal</h4>
-                                    </div>
-
+                                    {locations.map((location, index) => (
+                                                <div onClick={() => setPanel(true)} key={index} className="p-4 flex items-center my-2 justify-center gap-2 pt-10">
+                                                            <h2 className='text-2xl w-10 h-10 bg-[#eee] rounded-full p-4 flex justify-center items-center'>
+                                                                        <i className="ri-map-pin-fill"></i>
+                                                            </h2>
+                                                            <h4 className='text-4xl font-semibold'>{location}</h4>
+                                                </div>
+                                    ))}
                         </div>
-            )
-}
+            );
+};
 
-export default LocationSearch
+export default LocationSearch;
