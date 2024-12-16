5802576096325c47d7cba82c1e78e95bf5da3859
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-12-16 22:37:37 +0530

    getting two props

--- frontend/src/Components/RidePopUp.jsx
+++ frontend/src/Components/RidePopUp.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const RidePopUp = () => {
+const RidePopUp = ({ setRideOpenPop, setConfirmRidePopOpen }) => {
             return (
                         <div>
-                                    <h5 className='p-1 text-center w-[93%] absolute top-0' ><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
+                                    <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => setRideOpenPop(false)}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
                                     <h3 className='text-2xl font-semibold mb-5'>New Ride Available!</h3>
                                     <div className='flex items-center justify-between p-3 bg-yellow-400 rounded-lg mt-4'>
                                                 <div className='flex items-center gap-3 '>
@@ -37,11 +37,9 @@ const RidePopUp = () => {
                                                             </div>
                                                 </div>
                                                 <div className='mt-5 w-full '>
-                                                            <button className=' bg-green-600 w-full text-white font-semibold p-2 px-10 rounded-lg'>Accept</button>
+                                                            <button className=' bg-green-600 w-full text-white font-semibold p-2 px-10 rounded-lg' onClick={() => setConfirmRidePopOpen(true)}>Accept</button>
 
-                                                            <button className='mt-2 w-full bg-gray-300 text-gray-700 font-semibold p-2 px-10 rounded-lg'>Ignore</button>
-
-
+                                                            <button onClick={() => setRideOpenPop(false)} className='mt-2 w-full bg-gray-300 text-gray-700 font-semibold p-2 px-10 rounded-lg'>Ignore</button>
                                                 </div>
                                     </div>
                         </div>
