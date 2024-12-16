a51ddcbb414e94f11f60246d683791f5fa15e13f
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-12-16 22:58:42 +0530

    created some usestate

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import CaptainDetails from '../Components/CaptainDetails'
+import RidePopUp from '../Components/RidePopUp'
+import ConfirmRidePop from '../Components/ConfirmRidePop'
 
 const CaptainHome = () => {
+
+  const [RidePopOpen, setRideOpenPop] = useState(false)
+  const [confirmRidePopOpen, setConfirmRidePopOpen] = useState(true)
+
   return (
     <div className='h-screen'>
       <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
@@ -15,13 +22,13 @@ const CaptainHome = () => {
 
       </div>
       <div className='h-2/5 p-6'>
-        {/* <CaptainDetails /> */}
+        <CaptainDetails />
       </div>
-      <div className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
-
+      <div className={`fixed w-full z-10 bottom-0 ${RidePopOpen ? " translate-y-0" : " translate-y-full"}  transition-all duration-900 bg-white px-3 py-10 pt-12`}>
+        <RidePopUp setRideOpenPop={setRideOpenPop} setConfirmRidePopOpen={setConfirmRidePopOpen} />
       </div>
-      <div className='fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
-
+      <div className={`fixed w-full h-screen z-10 bottom-0 ${confirmRidePopOpen ? "translate-y-0" : "translate-y-full"}  transition-all duration-700 bg-white px-3 py-10 pt-12`}>
+        <ConfirmRidePop setConfirmRidePopOpen={setConfirmRidePopOpen} />
       </div>
     </div>
   )
